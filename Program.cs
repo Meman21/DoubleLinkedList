4052e14854757aeebea9840156c16e1c877c8e63
hanifi cicek <hanifi21@icloud.com>
2020-05-07 04:53:55 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinked myList = new DoublyLinked();
+            myList.Add(5);
+            myList.Add(24);
+            myList.Add(100);
+            myList.Add(53);
+            Console.WriteLine(myList.Remove(53));
+            myList.PrintValues();
+        }
+    }
+}
